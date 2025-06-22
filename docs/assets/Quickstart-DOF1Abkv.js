753ee38a4a1c9d20b1041fb260ef7d47b68f3144
jen <jenhseb@gmail.com>
2025-06-22 08:25:47 -0700

    add github link to docs

--- docs/assets/Quickstart-DOF1Abkv.js
+++ docs/assets/Quickstart-DOF1Abkv.js
@@ -1,11 +1,20 @@
-import{j as n}from"./iframe-BgshiAOW.js";import{useMDXComponents as i}from"./index-DaNmXBh5.js";function t(o){const e={a:"a",code:"code",em:"em",h1:"h1",h2:"h2",h3:"h3",h4:"h4",li:"li",ol:"ol",p:"p",pre:"pre",strong:"strong",ul:"ul",...i(),...o.components};return n.jsxs(n.Fragment,{children:[n.jsxs("div",{children:[n.jsx(e.h1,{id:"quickstart",children:"Quickstart"}),n.jsx(e.h2,{id:"prerequisites",children:"Prerequisites"}),n.jsxs(e.p,{children:["The library defaults to using OpenAI for LLM calls. Information on how to add other LLM provider (e.g. Anthropic) ",n.jsx(e.a,{href:"#llm",children:"here"}),". Or open an issue in Github."]}),n.jsxs(e.p,{children:["You need to add ",n.jsx(e.code,{children:"OPENAI_API_KEY"})," as an environment variable pointing to your ",n.jsx(e.a,{href:"https://help.openai.com/en/articles/4936850-where-do-i-find-my-openai-api-key",rel:"nofollow",children:"OpenAI API Key"}),"."]}),n.jsx(e.h2,{id:"out-of-the-box---3-components-needed",children:"Out of the Box - 3 Components Needed"}),n.jsxs(e.ol,{children:[`
+import{j as n}from"./iframe-Cuxpue41.js";import{useMDXComponents as i}from"./index-7-rZvKgK.js";function t(o){const e={a:"a",code:"code",em:"em",h1:"h1",h2:"h2",h3:"h3",h4:"h4",li:"li",ol:"ol",p:"p",pre:"pre",strong:"strong",ul:"ul",...i(),...o.components};return n.jsxs(n.Fragment,{children:[n.jsx("a",{href:"https://github.com/jenhseb/llm-tooltip",children:n.jsxs("svg",{role:"img",viewBox:"0 0 24 24",xmlns:"http://www.w3.org/2000/svg",height:20,width:20,children:[n.jsx("title",{children:"GitHub"}),n.jsx("path",{d:"M12 .297c-6.63 0-12 5.373-12 12 0 5.303 3.438 9.8 8.205 11.385.6.113.82-.258.82-.577 0-.285-.01-1.04-.015-2.04-3.338.724-4.042-1.61-4.042-1.61C4.422 18.07 3.633 17.7 3.633 17.7c-1.087-.744.084-.729.084-.729 1.205.084 1.838 1.236 1.838 1.236 1.07 1.835 2.809 1.305 3.495.998.108-.776.417-1.305.76-1.605-2.665-.3-5.466-1.332-5.466-5.93 0-1.31.465-2.38 1.235-3.22-.135-.303-.54-1.523.105-3.176 0 0 1.005-.322 3.3 1.23.96-.267 1.98-.399 3-.405 1.02.006 2.04.138 3 .405 2.28-1.552 3.285-1.23 3.285-1.23.645 1.653.24 2.873.12 3.176.765.84 1.23 1.91 1.23 3.22 0 4.61-2.805 5.625-5.475 5.92.42.36.81 1.096.81 2.22 0 1.606-.015 2.896-.015 3.286 0 .315.21.69.825.57C20.565 22.092 24 17.592 24 12.297c0-6.627-5.373-12-12-12"})]})}),`
+`,n.jsx("br",{}),`
+`,n.jsx(e.h1,{id:"quickstart",children:"Quickstart"}),`
+`,n.jsx(e.h2,{id:"prerequisites",children:"Prerequisites"}),`
+`,n.jsxs(e.p,{children:["The library defaults to using OpenAI for LLM calls. Information on how to add other LLM provider (e.g. Anthropic) ",n.jsx(e.a,{href:"#llm",children:"here"}),". Or open an issue in Github."]}),`
+`,n.jsxs(e.p,{children:["You need to add ",n.jsx(e.code,{children:"OPENAI_API_KEY"})," as an environment variable pointing to your ",n.jsx(e.a,{href:"https://help.openai.com/en/articles/4936850-where-do-i-find-my-openai-api-key",rel:"nofollow",children:"OpenAI API Key"}),"."]}),`
+`,n.jsx(e.h2,{id:"out-of-the-box---3-components-needed",children:"Out of the Box - 3 Components Needed"}),`
+`,n.jsxs(e.ol,{children:[`
 `,n.jsx(e.li,{children:n.jsx(e.code,{children:"SmartToolTip"})}),`
 `,n.jsx(e.li,{children:n.jsx(e.code,{children:"SmartToolTipProvider"})}),`
 `,n.jsx(e.li,{children:n.jsx(e.code,{children:"SmartToolTipHelp"})}),`
-`]}),n.jsxs(e.p,{children:["Wrap your app in the ",n.jsx(e.code,{children:"SmartToolTipProvider"}),":"]}),n.jsx(e.pre,{children:n.jsx(e.code,{className:"language-tsx",children:`<SmartToolTipProvider>
+`]}),`
+`,n.jsxs(e.p,{children:["Wrap your app in the ",n.jsx(e.code,{children:"SmartToolTipProvider"}),":"]}),`
+`,n.jsx(e.pre,{children:n.jsx(e.code,{className:"language-tsx",children:`<SmartToolTipProvider>
   <App />
 </SmartToolTipProvider>
-`})})]}),`
+`})}),`
 `,n.jsxs(e.p,{children:["And inside your app, wrap your components with the ",n.jsx(e.code,{children:"SmartToolTip"})," component:"]}),`
 `,n.jsxs(e.p,{children:["Add a ",n.jsx(e.code,{children:"description"})," prop to the ",n.jsx(e.code,{children:"SmartToolTip"})," component. This is internal-facing and will be sent to the LLM as context for the user's questions."]}),`
 `,n.jsx(e.pre,{children:n.jsx(e.code,{className:"language-tsx",children:`<SmartToolTip description={"Click this button to open up a greeting."}>
